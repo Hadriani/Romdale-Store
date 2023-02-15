3ce12db09822d512f7d428b5de0fcd204907d0e8
Adrian Păun <79643678+Hadriani@users.noreply.github.com>
2023-02-15 13:30:08 +0200

    Add files via upload

---
+++ pants-data.js
@@ -0,0 +1,292 @@
+const PANTS_MAP = [
+    {
+        id: 0,
+        name: 'Retro Mens Multi Pocket Outdoor Casual Jeans',
+        imageUrl: 'https://ups.aopcdn.com/s32398/goods/18882/0u4434ecd2c60a4736bac3552b99b4df98.jpg?x-oss-process=image/resize,limit_0,m_pad,w_600,h_600/format,webp/interlace,1/quality,q_80',
+        price: '56',
+        cartButton: 'ADD TO CART',
+        priceRange: '',
+        description: `
+Product Code:	SPT39F289KM
+Occasion:	Outdoor / Daily / Tactical / Casual
+Material:	Cotton
+Design Elements:	Zip Front
+Pant Length:	Shorts
+Pant Type:	Straight Leg
+Washing Mode:	Machine Wash
+Season:	All Season
+Style:	Casual / Tactical
+Waist Type:	Mid Waist`,
+        favoriteButton: '',
+        category: 'Jeans',
+    },
+    {
+        id: 1,
+        name: 'Mens Outdoor Wear-resistant Military Trousers',
+        imageUrl: 'https://ups.aopcdn.com/s46729/goods/18897/0u6dc2407b92e0412e96feb01e73188955.jpg?x-oss-process=image/resize,limit_0,m_pad,w_600,h_600/format,webp/interlace,1/quality,q_80',
+        price: '70',
+        cartButton: 'ADD TO CART',
+        priceRange: '',
+        description: `
+Product Code:	SPT39F289KM
+Occasion:	Outdoor / Daily / Tactical / Casual
+Material:	Cotton
+Design Elements:	Zip Front
+Pant Length:	Shorts
+Pant Type:	Straight Leg
+Washing Mode:	Machine Wash
+Season:	All Season
+Style:	Casual / Tactical
+Waist Type:	Mid Waist`,
+        favoriteButton: '',
+        category: 'Jeans',
+    },
+    {
+        id: 2,
+        name: 'Mens Casual Large Pocket Pants',
+        imageUrl: 'https://ups.aopcdn.com/s32398/goods/18882/0u322c399a86fb4eb0b64125281bfbcf82.png?x-oss-process=image/resize,limit_0,m_pad,w_600,h_600/format,webp/interlace,1/quality,q_80',
+        price: '48',
+        cartButton: 'ADD TO CART',
+        priceRange: '',
+        description: `
+Product Code:	SP43W7UCGR4
+Occasion:	Daily / Casual
+Gender:	Male
+Waist Style:	Zipper Fly
+Design:	Zipper
+Washing Mode:	Machine Wash
+Season:	Fall/Autumn / Winter
+Style:	Loose
+        `,
+        favoriteButton: '',
+        category: 'Jeans',
+    },
+    {
+        id: 3,
+        name: 'Mens Outdoor Multi Pocket Cotton Cargo Pants',
+        imageUrl: 'https://ups.aopcdn.com/s30885/goods/19363/1532uf556bb6529454020a7f471f8d5610528.jpg?x-oss-process=image/resize,limit_0,m_pad,w_600,h_600/format,webp/interlace,1/quality,q_80',
+        price: '42',
+        cartButton: 'ADD TO CART',
+        priceRange: '',
+        description: `
+Product Code:	SP230106B00H
+Occasion:	Leisure / Outdoor / Sport / Daily / Tactical / Casual
+Material:	Cotton
+Design Elements:	Zip Front / Pocket
+Pant Length:	Long Pants
+Pant Type:	Straight Leg
+Season:	Fall/Autumn / Winter / Spring
+Style:	Casual / Tactical / Vintage/Retro / Training Uniform / Running / Motorcycle Riding / Camping / On Foot / Mountaineering
+Waist Type:	Mid Waist`,
+        favoriteButton: '',
+        category: 'Jeans',
+    },
+    {
+        id: 4,
+        name: 'Cotton Sports Cargo Shorts',
+        imageUrl: 'https://ups.aopcdn.com/s49186/goods/19065/1437ud7b8d58ecf4c49a4b586ad05008a6980.jpg?x-oss-process=image/resize,limit_0,m_pad,w_600,h_600/format,webp/interlace,1/quality,q_80',
+        price: '40',
+        cartButton: 'ADD TO CART',
+        priceRange: '',
+        description: `
+Product Code:	SP220119HKKK
+Occasion:	Leisure / Outdoor / Home / Beach / Sport / Daily / Tactical
+Material:	Cotton
+Design Elements:	Utility / Pocket
+Pant Length:	Shorts
+Pant Type:	Straight Leg
+Washing Mode:	Machine Wash
+Season:	Summer / Spring
+Style:	Casual / Tactical / Basic / Training Uniform / Running / Camping / On Foot / Mountaineering
+Waist Type:	Mid Waist`,
+        favoriteButton: '',
+        category: 'Jeans',
+    },
+    {
+        id: 5,
+        name: 'Mens Outdoor Camouflage Casual Shorts',
+        imageUrl: 'https://ups.aopcdn.com/s48100/goods/18981/-2u0a0fa4937025420e948636ff52199a29.jpg?x-oss-process=image/resize,limit_0,m_pad,w_600,h_600/format,webp/interlace,1/quality,q_80',
+        price: '53',
+        cartButton: 'ADD TO CART',
+        priceRange: '',
+        description: `
+Product Code:	SP237O2HQX5
+Occasion:	Daily / Outdoor
+Gender:	Male
+Material:	Cotton Blend
+Waist Style:	Zipper Fly
+Design Elements:	Zip Front
+Washing Mode:	Cold Hand Wash
+Pant Length:	Shorts
+Pant Type:	Straight Leg
+Season:	Fall/Autumn / Summer / Spring
+Style:	Loose / Tactical
+Waist Type:	Mid Waist`,
+        favoriteButton: '',
+        category: 'Jeans',
+    },
+    {
+        id: 6,
+        name: 'Western Yellowstone Print Casual Pants',
+        imageUrl: 'https://ups.aopcdn.com/s46729/goods/18897/0u6dc2407b92e0412e96feb01e73188955.jpg?x-oss-process=image/resize,limit_0,m_pad,w_600,h_600/format,webp/interlace,1/quality,q_80',
+        price: '30',
+        cartButton: 'ADD TO CART',
+        priceRange: '',
+        description: `
+Product Code:	SP230203S9M4
+Occasion:	Leisure / Outdoor / Sport / Daily / Tactical / Casual
+Material:	Terry Cloth
+Design Elements:	Print
+Pant Length:	Long Pants
+Pant Type:	Straight Leg
+Season:	Fall/Autumn / Winter / Spring
+Style:	Fitness / Casual / Tactical / Training Uniform / Running
+Waist Type:	Mid Waist`,
+        favoriteButton: '',
+        category: 'Jeans',
+    },
+    {
+        id: 7,
+        name: 'Vintage Western Yellowstone Casual Pants',
+        imageUrl: 'https://ups.aopcdn.com/s30222/goods/19394/-2u217e67441f1145ef9546a3647ff76080.jpg?x-oss-process=image/resize,limit_0,m_pad,w_600,h_600/format,webp/interlace,1/quality,q_80',
+        price: '28',
+        cartButton: 'ADD TO CART',
+        priceRange: '',
+        description: `
+Product Code:	SP230203A80Q
+Occasion:	Leisure / Outdoor / Sport / Daily / Tactical / Casual
+Material:	Terry Cloth
+Design Elements:	Print
+Pant Length:	Long Pants
+Pant Type:	Straight Leg
+Season:	Fall/Autumn / Winter / Spring
+Style:	Fitness / Casual / Tactical / Training Uniform / Running 
+Waist Type:	Mid Waist`,
+        favoriteButton: '',
+        category: 'Jeans',
+    },
+    {
+        id: 8,
+        name: 'Mens Outdoor Retro Crazy Skull Trousers',
+        imageUrl: 'https://ups.aopcdn.com/s48100/goods/18981/-2u32602aa7b4b643f0956d568a963a0720.jpg?x-oss-process=image/resize,limit_0,m_pad,w_600,h_600/format,webp/interlace,1/quality,q_80',
+        price: '35',
+        cartButton: 'ADD TO CART',
+        priceRange: '',
+        description: `
+Product Code:	SP2110223M5S
+Occasion:	Leisure / Outdoor / Sport / Tactical / Casual
+Material:	Cotton Blend
+Design Elements:	Print
+Pant Length:	Long Pants
+Pant Type:	Skinny
+Season:	Fall/Autumn / Winter / Spring
+Style:	Casual / Tactical / Vintage/Retro
+Waist Type:	Mid Waist`,
+        favoriteButton: '',
+        category: 'Jeans',
+    },
+    {
+        id: 9,
+        name: 'Mens Outdoor Skull Pocket Tactical Shorts',
+        imageUrl: 'https://ups.aopcdn.com/s30885/goods/19103/-2u2b852c7ffad84fed967b625c9b871ef1.jpg?x-oss-process=image/resize,limit_0,m_pad,w_600,h_600/format,webp/interlace,1/quality,q_80',
+        price: '27',
+        cartButton: 'ADD TO CART',
+        priceRange: '',
+        description: `
+Product Code:	SP220421EL84
+Occasion:	Leisure / Outdoor / Sport / Daily / Tactical / Casual
+Material:	Polyester
+Design Elements:	Print
+Pant Length:	Shorts
+Pant Type:	Straight Leg
+Season:	Summer / Fall/Autumn / Winter / Spring
+Style:	Fitness / Casual / Motorcycle Riding / Camping / On Foot / Mountaineering
+Waist Type:	Mid Waist`,
+        favoriteButton: '',
+        category: 'Jeans',
+    },
+    {
+        id: 10,
+        name: 'Vintage American Flag Print Casual Trousers',
+        imageUrl: 'https://ups.aopcdn.com/s30885/goods/19261/1535u41b3ddd43f8641109c4b5aa8e77799e5.jpg?x-oss-process=image/resize,limit_0,m_pad,w_600,h_600/format,webp/interlace,1/quality,q_80',
+        price: '40',
+        cartButton: 'ADD TO CART',
+        priceRange: '',
+        description: `
+Product Code:	SP22092694HK
+Occasion:	Leisure / Outdoor
+Material:	Polyester
+Design Elements:	Print
+Pant Length:	Long Pants
+Pant Type:	Straight Leg
+Season:	Summer / Fall/Autumn / Winter / Spring
+Style:	Casual
+Waist Type:	Mid Waist`,
+        favoriteButton: '',
+        category: 'Jeans',
+    },
+    {
+        id: 11,
+        name: 'Camouflage Retro Casual Cargo Pants',
+        imageUrl: 'https://ups.aopcdn.com/s24852/goods/18856/-2u003eed4b8bc0443a983b8c8b99b96e4f.jpg?x-oss-process=image/resize,limit_0,m_pad,w_600,h_600/format,webp/interlace,1/quality,q_80',
+        price: '38',
+        cartButton: 'ADD TO CART',
+        priceRange: '',
+        description: `
+Product Code:	SP210816ASA8
+Occasion:	Outdoor / Sport / Daily
+Material:	Woven Fabric
+Design Elements:	Pocket
+Pant Length:	Long Pants
+Pant Type:	Skinny
+Season:	All Season
+Style:	Casual / Tactical
+Waist Type:	Mid Waist`,
+        favoriteButton: '',
+        category: 'Jeans',
+    },
+    {
+        id: 12,
+        name: 'Tactical Pocket Wear-resistant Casual Pants',
+        imageUrl: 'https://ups.aopcdn.com/s24862/goods/18844/818ud7c6aea160e84b1baf06ee4120c03576.jpg?x-oss-process=image/resize,limit_0,m_pad,w_600,h_600/format,webp/interlace,1/quality,q_80',
+        price: '46',
+        cartButton: 'ADD TO CART',
+        priceRange: '',
+        description: `
+Product Code:	SP2108040DKO
+Wearing Occasion:	Outdoor
+Laundry Guide:	Cold Hand Wash
+Pants Type:	Straight-leg Pants
+Fabric:	Woven Fabric
+Design Elements:	Practical Tooling / Pocket
+Pants Length:	Trousers
+Season:	All Season
+Style:	Outdoor Tactical Style
+Waist Type:	Middle Waist`,
+        favoriteButton: '',
+        category: 'Jeans',
+    },
+    {
+        id: 13,
+        name: 'Tactical Retro Printed Casual Pants Trousers',
+        imageUrl: 'https://ups.aopcdn.com/s24852/goods/19228/1082ua8ef4986cc704db3aa903e98bd936ebd.jpg?x-oss-process=image/resize,limit_0,m_pad,w_600,h_600/format,webp/interlace,1/quality,q_80',
+        price: '42',
+        cartButton: 'ADD TO CART',
+        priceRange: '',
+        description: `
+Product Code:	SPCMVHJVV1A
+Occasion:	Leisure / Outdoor
+Material:	Polyester
+Design Elements:	Print
+Pant Length:	Long Pants
+Pant Type:	Skinny
+Season:	All Season
+Style:	Outdoor / Outdoor Sports / Training Outdoor Tactics
+Waist Type:	Mid Waist`,
+        favoriteButton: '',
+        category: 'Jeans',
+    },
+
+];
+
+export default PANTS_MAP;
